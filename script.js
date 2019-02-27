c7dd637caf83749f91fd0f9e2fdf1c5f9e95c3fe
mikesuchor <mikesuchor@gmail.com>
2019-02-26 23:43:56 -0700

    Removed unneeded code

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 var port = 8080;
-var regex = /(https?:\/\/)([\da-zA-Z\-]+).([\da-zA-z\-]+).([\da-zA-z\-\.]+)/;
-
-app.get('/:url', function(req, res) {
-    console.log(regex.test(req.params.url));
-    res.end(req.params.url);
-});
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
